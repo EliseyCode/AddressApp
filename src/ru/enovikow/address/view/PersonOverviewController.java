5e1f2f9b088b96d16406f46bbef36a4f6d204fcb
Elisey Novikov <enovikow6@gmail.com>
2017-09-26 02:22:05 +0300

    Add realisation delete button. Add form PersonEditDialog.fxml form

--- src/ru/enovikow/address/view/PersonOverviewController.java
+++ src/ru/enovikow/address/view/PersonOverviewController.java
@@ -1,11 +1,13 @@
 package ru.enovikow.address.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import ru.enovikow.address.MainApp;
 import ru.enovikow.address.model.Person;
+import ru.enovikow.address.util.DateUtil;
 
 public class PersonOverviewController {
 
@@ -38,12 +40,43 @@ public class PersonOverviewController {
     }
 
     /**
+     *
+     * @param person
+     */
+    private void showPersonDetails(Person person) {
+       if(person != null) {
+           // Заполняем метки информацией из объекта person
+           firstNameLabel.setText(person.getFirstName());
+           lastNameLabel.setText(person.getLastName());
+           streetLabel.setText(person.getStreet());
+           postalLabel.setText(Integer.toString(Integer.parseInt(person.getPostalCode())));
+           cityLabel.setText(person.getCity());
+           birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+       } else {
+           // Если person = null убираем весь текст
+           firstNameLabel.setText("");
+           lastNameLabel.setText("");
+           streetLabel.setText("");
+           postalLabel.setText("");
+           cityLabel.setText("");
+           birthdayLabel.setText("");
+       }
+    }
+
+    /**
      * Инициализация класса-контрроллера. Метод вызывается автоматически после того как fxml-файл будет загружен.
      */
     @FXML
     private void initialize() {
+        // Инициализация таблицы адресатов с двумя столбцами.
         firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().getFirstNameProperty());
         lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().getLastNameProperty());
+
+        // Очистка дополнительной информации об адресате.
+        showPersonDetails(null);
+
+        // Слушаем изменения выбора и при изменении отображаем доп. информацию об адресате.
+        personTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showPersonDetails(newValue));
     }
 
     /**
@@ -55,5 +88,24 @@ public class PersonOverviewController {
 
         // Добавление в таблицу данных из наблюдаемого списка
         personTableView.setItems(mainApp.getPersonData());
+    }
+
+    /**
+     * Вызывается когда пользователь кликает по кнопке удаления.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTableView.getSelectionModel().getSelectedIndex();
+        if(selectedIndex >= 0) {
+            personTableView.getItems().remove(selectedIndex);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("Ошибка выбора");
+            alert.setHeaderText("Контакт не выбран");
+            alert.setContentText("Для удаления контакта из базы выберете пользователя из списка");
+
+            alert.showAndWait();
+        }
     }
 }
